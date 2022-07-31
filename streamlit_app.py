099ae58c7d50b5b2e1a4b64dcdbd7aa41c8a60a0
jwusko74 <101141471+jwusko74@users.noreply.github.com>
2022-07-31 19:53:31 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,9 @@
 import streamlit
 
-streamlit.title('My Parents New Healthy Diner')
+streamlit.title(#'My Parents New Healthy Diner')
 
-streamlit.title('Breakfast Menu')
+streamlit.title(##'Breakfast Menu')
 
-streamlit.title('Omega 3 Blueberry Pancakes')
+streamlit.title(######'Omega 3 Blueberry Pancakes')
 streamlit.title('Kale and Spinach Smoothie')
 streamlit.title('Hard-Boiled Free Range Egg')
