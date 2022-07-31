5d8ce1922ad1f986cef01999de952610d817d2f1
jwusko74 <101141471+jwusko74@users.noreply.github.com>
2022-07-31 19:54:01 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,5 @@ streamlit.title(#'My Parents New Healthy Diner')
 streamlit.title(##'Breakfast Menu')
 
 streamlit.title(######'Omega 3 Blueberry Pancakes')
-streamlit.title('Kale and Spinach Smoothie')
-streamlit.title('Hard-Boiled Free Range Egg')
+streamlit.title((######''Kale and Spinach Smoothie')
+streamlit.title((######''Hard-Boiled Free Range Egg')
