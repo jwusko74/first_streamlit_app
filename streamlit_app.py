44ed35ab2e6566a3ab7379a3777a3c35c569b368
jwusko74 <101141471+jwusko74@users.noreply.github.com>
2022-08-02 18:57:01 -0400

    Update streamlit_app.py
    added lunch menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,11 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-
 streamlit.text('Omega 3 Blueberry Pancakes')
 streamlit.text('Kale and Spinach Smoothie')
 streamlit.text('Hard-Boiled Free Range Egg')
+
+streamlit.header('Lunch Menu')
+streamlit.text('Tuna Sandwich')
+streamlit.text('Chef Salad')
+streamlit.text('Power Protein Smoothie')
